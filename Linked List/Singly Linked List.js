0fa0b3689fb468d090844d02e2de0075b1c1cb1e
Sharif Al Hosafy <sherifismail44@gmail.com>
2022-06-17 16:47:23 +0200

    adding helper methods and testing the LL

--- Linked List/Singly Linked List.js
+++ Linked List/Singly Linked List.js
@@ -109,18 +109,88 @@ class LL {
     }
     return -1;
   }
+
+  display() {
+    if (this.size == 0) console.log("The List Is Empty");
+    else {
+      let curr = this.head;
+      let str = "";
+      while (curr != null) {
+        str += curr.element + " ";
+        curr = curr.next;
+      }
+      console.log(str);
+    }
+  }
+  indexOf(element) {
+    let idx = 0;
+    let curr = this.head;
+
+    while (curr != null) {
+      if (curr.element == element) return idx;
+      curr = curr.next;
+      idx++;
+    }
+    return -1;
+  }
+
+  isEmpty() {
+    return this.size == 0;
+  }
+
+  size_of_list() {
+    return this.size;
+  }
 }
 
-let list = new LL();
-list.add(4);
-list.add(7);
-list.add(2);
-list.add(3);
-list.add(5);
-console.log(list);
-list.removeFrom(0);
-console.log(list);
-// list.removeElement(4);
-// console.log(list);
-// list.removeElement(7);
-// console.log(list);
+/////////////////////////////////// Test Cases ////////////////////////////////////////////////////////////
+
+// creating an object for the
+// Linkedlist class
+var ll = new LL();
+
+// testing isEmpty on an empty list
+// returns true
+console.log(ll.isEmpty());
+
+// adding element to the list
+ll.add(10);
+
+// prints 10
+ll.display();
+
+// returns 1
+console.log(ll.size_of_list());
+
+// adding more elements to the list
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+
+// returns 10 20 30 40 50
+ll.display();
+
+// prints 50 from the list
+console.log("is element removed ? " + ll.removeElement(50));
+
+// prints 10 20 30 40
+ll.display();
+
+// returns 3
+console.log("Index of 40 is ---> " + ll.indexOf(40));
+
+// insert 60 at second position
+// ll contains 10 20 60 30 40
+ll.insertAt(60, 2);
+
+ll.display();
+
+// returns false
+console.log("is List Empty ? " + ll.isEmpty());
+
+// remove 3rd element from the list
+console.log(ll.removeFrom(3));
+
+// prints 10 20 60 40
+ll.display();
