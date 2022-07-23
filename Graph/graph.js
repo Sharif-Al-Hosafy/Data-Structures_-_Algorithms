b5345690d9e2f84049ed4cf99b766fb391deb6ed
Sharif Al Hosafy <sherifismail44@gmail.com>
2022-07-23 11:41:25 +0200

    graph with bfs traversal

---
+++ Graph/graph.js
@@ -0,0 +1,101 @@
+class Graph {
+  // adjacent list
+  constructor(noOfVertices) {
+    this.noOfVertices = noOfVertices;
+    this.AdjList = new Map();
+  }
+
+  // add vertex to the graph
+  addVertex(v) {
+    // initialize the adjacent list with a
+    // null array
+    this.AdjList.set(v, []);
+  }
+
+  // add edge to the graph
+  addEdge(v, w) {
+    // get the list for vertex v and put the
+    // vertex w denoting edge between v and w
+    this.AdjList.get(v).push(w);
+
+    // undirected graph
+    // add an edge from w to v also
+    this.AdjList.get(w).push(v);
+  }
+
+  // Prints the vertex and adjacency list
+  printGraph() {
+    // get all the vertices
+    var get_keys = this.AdjList.keys();
+
+    // iterate over the vertices
+    for (var i of get_keys) {
+      // great the corresponding adjacency list
+      // for the vertex
+      var get_values = this.AdjList.get(i);
+      var conc = "";
+      for (var j of get_values) conc += j + " ";
+      console.log(i + " -> " + conc);
+    }
+  }
+
+  // bfs(v)
+  bfs(start) {
+    // create a visited object
+    let visited = {};
+    let conc = "BFS";
+
+    // Create a queue
+    var q = [];
+
+    visited[start] = true;
+    q.push(start);
+
+    while (q.length !== 0) {
+      let getQueueElement = q.shift();
+      let adjacentNodes = this.AdjList.get(getQueueElement);
+      conc += " -> " + getQueueElement;
+      for (let node of adjacentNodes) {
+        if (!visited[node]) {
+          visited[node] = true;
+          q.push(node);
+        }
+      }
+    }
+    console.log(conc);
+  }
+
+  // dfs(v)
+  dfs(start) {}
+}
+
+// Using the above implemented graph class
+var g = new Graph(6);
+var vertices = ["A", "B", "C", "D", "E", "F"];
+
+// adding vertices
+for (var i = 0; i < vertices.length; i++) {
+  g.addVertex(vertices[i]);
+}
+
+// adding edges
+g.addEdge("A", "B");
+g.addEdge("A", "D");
+g.addEdge("A", "E");
+g.addEdge("B", "C");
+g.addEdge("D", "E");
+g.addEdge("E", "F");
+g.addEdge("E", "C");
+g.addEdge("C", "F");
+
+// prints all vertex
+
+// A -> B D E
+// B -> A C
+// C -> B E F
+// D -> A E
+// E -> A D F C
+// F -> E C
+//g.printGraph();
+
+g.bfs("A");
